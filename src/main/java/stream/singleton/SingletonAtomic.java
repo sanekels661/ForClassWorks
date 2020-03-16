bee1ec20150cc4ee6415926c94129f0267f89042
sanekels661 <elsakov982@inbox.ru>
2020-03-16 16:29:35 +0300

    firstTest

--- src/main/java/stream/singleton/SingletonAtomic.java
+++ src/main/java/stream/singleton/SingletonAtomic.java
@@ -1,19 +1,22 @@
 package stream.singleton;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class SingletonAtomic {
 
     private static AtomicReference<SingletonAtomic> INSTANCE;
-
-    private SingletonAtomic(){
+    private final int id;
+    private static final AtomicInteger counter = new AtomicInteger(0);
+    private SingletonAtomic(int id){
+        this.id = id;
         INSTANCE = new AtomicReference<>();
     }
     public static SingletonAtomic getInstance(){
 
         if(INSTANCE == null){
-            SingletonAtomic s = new SingletonAtomic();
-            INSTANCE.compareAndSet(s, null);
+            SingletonAtomic s = new SingletonAtomic(counter.getAndIncrement());
+            INSTANCE.compareAndSet(null, s);
         }
         return getInstance();
     }
---
+++ src/test/java/stream/singleton/SingletonAtomicTest.java
@@ -0,0 +1,21 @@
+package stream.singleton;
+
+import org.junit.Test;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import static org.junit.Assert.*;
+
+public class SingletonAtomicTest {
+
+    @Test
+    public void testSingle(){
+        Executor ssss = Executors.newFixedThreadPool(100000);
+        for (int i = 0; i < 100000; i++){
+            
+        }
+
+    }
+
+}
